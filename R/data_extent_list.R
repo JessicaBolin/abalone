6660c41567fae9056faee2555d82f51ae29a890c
Jessica A. Bolin <37993300+JessicaBolin@users.noreply.github.com>
2025-06-13 10:29:36 -0700

    New data

---
+++ R/data_extent_list.R
@@ -0,0 +1,15 @@
+#' List of extents for case study regions
+#'
+#' This object contains bounding boxes (latitudes and longitudes) for key study regions used in the refugia analysis. These can be used with `terra::ext()` for vizualisation.
+#'
+#' \strong{Available extents:}
+#' \itemize{
+#'   \item \strong{Monterey Bay}: \code{monterey_bay}
+#'   \item \strong{Fort Bragg}: \code{fort_bragg}
+#'   \item \strong{Channel Islands}: \code{channel_islands}
+#'   \item \strong{San Francisco}: \code{san_francisco}
+#' }
+#' @source Custom defined by project team
+#' @examples
+#' extent_list
+"extent_list"
