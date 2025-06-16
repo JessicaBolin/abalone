12a2aeed44c535404118329019d2eb6c175a6cf5
Jessica A. Bolin <37993300+JessicaBolin@users.noreply.github.com>
2025-06-16 16:10:16 -0700

    Try to fix bug, failed :(

--- R/persist_rast.R
+++ R/persist_rast.R
@@ -48,9 +48,10 @@ persist_rast <- function(model = c("ens", "gfdltv", "hadtv", "ipsltv"),
     message("Raster saved to: ", normalizePath(save_pathh))
   } else if (interactive()) {
     # Developer convenience: save to inst/extdata
-    dev_path <- file.path(usethis::proj_path(), "inst", "extdata",
+    dev_path <- file.path(suppressMessages(usethis::proj_path(quiet = getOption("usethis.quiet", default = FALSE)),
+                                           "inst", "extdata",
                           paste0(model2, "_persistence_", persist_thresh,
-                                 "_", min(yr_range), "-", max(yr_range), ".tif"))
+                                 "_", min(yr_range), "-", max(yr_range), ".tif")))
     terra::writeRaster(persistence_raster, dev_path, overwrite = TRUE)
     message("Developer raster saved to: ", normalizePath(dev_path))
   }
