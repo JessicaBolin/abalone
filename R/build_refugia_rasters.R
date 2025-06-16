8158b52306bacab7eae09782be9295e9dcb7c2c7
Jessica A. Bolin <37993300+JessicaBolin@users.noreply.github.com>
2025-06-16 14:03:32 -0700

    Update refugia_function wiht save_path

--- R/build_refugia_rasters.R
+++ R/build_refugia_rasters.R
@@ -10,6 +10,7 @@
 #' @param yrend Integer. End year. Defaults to 2100.
 #' @param thresh Integer. Temporal threshold (%) used for defining annual refugia. Choose either 50 (liberal) or 95% (conservative).
 #' @param progress Logical. Show progress bar? Default is TRUE.
+#' @param save_path Character. Directory to save output to. Optional, set to NULL if not needed.
 
 #' @return Produces a `terra::rast()` object of binary refugia (0/1), for the ESM specified, for all years between `yrst` and `yrend`.
 #' @export
@@ -19,17 +20,18 @@
 #'
 #' @examples
 #' ens_refugia <- build_refugia_rasters(percentdays = abalone::percentdays,
-#' esm = "ens", yrst = 1990, yrend = 2100, thresh = 50, progress = FALSE)
+#' esm = "ens", yrst = 1990, yrend = 2100, thresh = 50, progress = FALSE,
+#' save_path = NULL)
 #' ens_refugia
 #' terra::plot(ens_refugia[[98]], main = "Refugia in 2088")
-#'
 #'
 build_refugia_rasters <- function(percentdays = abalone::percentdays,
                                   esm = c("ens", "gfdltv", "hadtv", "ipsltv"),
                                   yrst = 1990,
                                   yrend = 2100,
                                   thresh = c(50, 95),
-                                  progress = TRUE) {
+                                  progress = TRUE,
+                                  save_path = NULL) {
 
   model2 <- ifelse(esm == "^zoom", "zoom", esm)
 
@@ -73,16 +75,48 @@ build_refugia_rasters <- function(percentdays = abalone::percentdays,
 
   if (progress) close(pb)
 
-  return(model_rast)
+  # Save raster to file
+  if (!is.null(save_path)) {
+    dir_to_save <- dirname(save_path)
+    if (!dir.exists(dir_to_save)) dir.create(dir_to_save, recursive = TRUE)
+    save_pathh <- paste0(save_path, "/", esm, "_refugia_", thresh, ".tif")
+    terra::writeRaster(model_rast, save_pathh, overwrite = TRUE)
+    message("Raster saved to: ", normalizePath(save_pathh))
+  } else if (interactive()) {
+    # Developer convenience: save to inst/extdata
+    dev_path <- file.path(usethis::proj_path(), "inst", "extdata",
+                          paste0(esm, "_refugia_", thresh, ".tif"))
+    terra::writeRaster(model_rast, dev_path, overwrite = TRUE)
+    message("Developer raster saved to: ", normalizePath(dev_path))
+  }
 
+  return(model_rast)
 }
 
+
+# # For internal package development (no save path argument!)
+# build_refugia_rasters(
+#   percentdays = abalone::percentdays,
+#   esm = "ens",
+#   yrst = 1990,
+#   yrend = 2100,
+#   thresh = 50,
+#   progress = TRUE)
+# build_refugia_rasters(percentdays = abalone::percentdays, esm = "ens", yrst = 1990, yrend = 2100, thresh = 95, progress = TRUE)
+# build_refugia_rasters(percentdays = abalone::percentdays, esm = "gfdltv", yrst = 1990, yrend = 2100, thresh = 50, progress = TRUE)
+# build_refugia_rasters(percentdays = abalone::percentdays, esm = "gfdltv", yrst = 1990, yrend = 2100, thresh = 95, progress = TRUE)
+# build_refugia_rasters(percentdays = abalone::percentdays, esm = "hadtv", yrst = 1990, yrend = 2100, thresh = 50, progress = TRUE)
+# build_refugia_rasters(percentdays = abalone::percentdays, esm = "hadtv", yrst = 1990, yrend = 2100, thresh = 95, progress = TRUE)
+# build_refugia_rasters(percentdays = abalone::percentdays, esm = "ipsltv", yrst = 1990, yrend = 2100, thresh = 50, progress = TRUE)
+# build_refugia_rasters(percentdays = abalone::percentdays, esm = "ipsltv", yrst = 1990, yrend = 2100, thresh = 95, progress = TRUE)
+
 #
-# build_refugia_rasters(percentdays,
-#                       esm = "ens",
-#                       thresh = 50,
-#                       yrst = 1990,
-#                       yrend = 2100)
-
-
-
+# # For users
+# build_refugia_rasters(
+#   percentdays = abalone::percentdays,
+#   esm = "ens",
+#   yrst = 1990,
+#   yrend = 2100,
+#   thresh = 50,
+#   progress = TRUE,
+#   save_path = "/Users/admin/Desktop/untitled folder") #specify a folder
